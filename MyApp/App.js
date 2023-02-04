093c66649abfd4798b8b394193ad287ab47db954
Swendart <sophie.curinier@etu.emse.fr>
2023-02-04 18:47:23 -0500

    Revert "Create an example component"
    This reverts commit 2f31b2aba152d501e50928cb837fc36c658be89b.

--- MyApp/App.js
+++ MyApp/App.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Schedule from './components/Schedule.js';
 
 function HomeScreen({ navigation }) {
   return (
@@ -10,7 +9,7 @@ function HomeScreen({ navigation }) {
       <Text>Home Screen</Text>
       <Button
         title="Go to Details"
-        onPress={() => navigation.navigate('Schedule')}
+        onPress={() => navigation.navigate('Details')}
       />
     </View>
   );
@@ -19,10 +18,11 @@ function HomeScreen({ navigation }) {
 function DetailsScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>test Screen</Text>
+      <Text>Details Screen</Text>
     </View>
   );
 }
+
 
 
 const Stack = createNativeStackNavigator();
@@ -33,7 +33,6 @@ function App() {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
-        <Stack.Screen name="Schedule" component={Schedule} />
       </Stack.Navigator>
     </NavigationContainer>
   );
