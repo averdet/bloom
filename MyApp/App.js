d0d3f8faad0a0922af6e43cd64685044cbe52c08
averdet <alex.verdet@outlook.fr>
2023-02-04 18:59:40 -0500

    working fab

--- MyApp/App.js
+++ MyApp/App.js
@@ -2,18 +2,50 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FloatingAction } from "react-native-floating-action";
 import { format } from 'react-string-format';
 
 
 const name = "Alexandre";
+const actions = [
+  {
+    text: "Accessibility",
+    icon: require("./images/ic_accessibility_white.png"),
+    name: "bt_accessibility",
+    position: 2
+  },
+  {
+    text: "Language",
+    icon: require("./images/ic_accessibility_white.png"),
+    name: "bt_language",
+    position: 1
+  },
+  {
+    text: "Location",
+    icon: require("./images/ic_accessibility_white.png"),
+    name: "bt_room",
+    position: 3
+  },
+  {
+    text: "Video",
+    icon: require("./images/ic_accessibility_white.png"),
+    name: "bt_videocam",
+    position: 4
+  }
+];
 function HomeVue() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Bonjour {name} !</Text>
+      <FloatingAction
+        actions={actions}
+        onPressItem={name => {
+          console.log(`selected button: ${name}`);
+        }}
+      />
     </View>
   );
 }
-
 
 function ScheduleVue() {
   return (
