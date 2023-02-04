a03c25e57e470558b648712d2d8080db20cb0ee4
Swendart <sophie.curinier@etu.emse.fr>
2023-02-04 18:53:23 -0500

    Correct timeline

--- MyApp/components/Schedule.js
+++ MyApp/components/Schedule.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
-import Timeline from 'react-native-timeline-flatlist'
+import Timeline from 'react-native-timeline-flatlist';
 
 function ScheduleVue() {
     return(
