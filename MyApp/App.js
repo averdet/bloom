7e8baa0acba9ac41e500d54713fa4f3df2b46001
averdet <alex.verdet@outlook.fr>
2023-02-04 18:01:58 -0500

    created 4 vues

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,41 +1,55 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { format } from 'react-string-format';
 
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
-  );
-}
 
-function DetailsScreen() {
+const name = "Alexandre";
+function HomeVue() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Bonjour {name} !</Text>
     </View>
   );
 }
 
 
+function ScheduleVue() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Programme de la journée</Text>
+    </View>
+  );
+}
 
-const Stack = createNativeStackNavigator();
+function ProgressVue() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
 
-function App() {
+function StatsVue() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Accueil" component={HomeVue} />
+        <Tab.Screen name="Programme" component={ScheduleVue} />
+        <Tab.Screen name="Progrès" component={ProgressVue} />
+        <Tab.Screen name="Stats" component={StatsVue} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
-export default App;
