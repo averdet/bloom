cb70a9332eea48b6063cb33797505517c12dce89
averdet <alex.verdet@outlook.fr>
2023-02-05 01:10:31 -0500

    first calls to api

--- MyApp/components/NewProject.js
+++ MyApp/components/NewProject.js
@@ -1,29 +1,107 @@
 import * as React from 'react';
-import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView } from 'react-native';
+import { useState } from 'react';
+import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView, Button } from 'react-native';
+import axios from 'axios'
 
-const OpenAiApiKey = "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";
+function ProjectVue({ navigator }) {
 
-function ProjectVue({ navigator}) {
+  const apiKey = "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";
+  const apiUrl = "https://api.openai.com/v1/engines/text-davinci-003/completions";
+  const [data, setData] = useState([]);
+  const [textInput, setTextInput] = useState('');
+  const [textPrompt, setTextPrompt] = useState('');
+  const [textOutput, setTextOutput] = useState('');
+  const [promptResponse, setPromptResponse] = useState('');
+  const [events, setEvents] = useState([]);
+
+
+  const handleCheckSend = async () => {
+    const prompt = `Une reformulation synthétique en une seule phrase du projet suivant ${textInput} est `;
+    console.log(prompt);
+    const response = await axios.post(apiUrl, {
+      prompt: prompt,
+      max_tokens: 216,
+      temperature: 0.3,
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`
+      }
+    });
+    const text = response.data.choices[0].text;
+    setTextOutput(text)
+    console.log(`API Call Reponse: ${text}`)
+    setData([...data, {type: 'user', 'text': textInput}, {type: 'bot', 'text': textInput}]);
+    setTextInput('');
+  };
+
+  const handleObjectiveSend = async () => {
+    setPromptResponse('');
+    const prompt = `Pour atteindre l'objectif suivant ${textOutput}, un bon planning habdomadaire est le suivant, avec les durées horaires à consacrer. Ennonce seulement et simplement quoi faire chaque jour pendant quelle durée `;
+    console.log(prompt);
+    const response = await axios.post(apiUrl, {
+      prompt: prompt,
+      max_tokens: 1024,
+      temperature: 0.3,
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`
+      }
+    });
+    const text = response.data.choices[0].text;
+    setPromptResponse(text)
+    console.log(`API Call Reponse: ${text}`)
+    setData([...data, {type: 'user', 'text': textInput}, {type: 'bot', 'text': textInput}]);
+    setTextInput('');
+    extractEvents();
+  };
+
+  const extractEvents = () => {
+    const extractedEvents = textOutput.match(
+      /\b\d{1,2}\/\d{1,2}\/\d{4}\b/g
+    );
+    setEvents(extractedEvents);
+    console.log(extractEvents);
+  };
+
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        {/* Write a task */}
-        {/* Uses a keyboard avoiding view which ensures the keyboard does not cover the items on screen */}
         <KeyboardAvoidingView 
-            behavior={Platform.OS === "ios" ? "padding" : "height"}
             style={styles.writeTaskWrapper}
         >
-            <TextInput style={styles.input} placeholder={'Décris ton projet'} value={null} onChangeText={text => setTask(text)} />
-            <TouchableOpacity onPress={() => handleAddTask()}>
-            <View style={styles.addWrapper}>
+            <TextInput style={styles.input} placeholder={'Décris ton projet'} value={textInput} onChangeText={text => setTextInput(text)}/>
+            <TouchableOpacity onPress={() => handleCheckSend()}>
+            <View style={styles.submitButton}>
                 <Text style={styles.addText}>✓</Text>
             </View>
             </TouchableOpacity>
         </KeyboardAvoidingView>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          {promptResponse.length < 1 ?               <Text style={styles.output} numberOfLines={25}>
+                Peux-tu confirmer que j'ai bien compris ton objectif:
+              {textOutput}
+              </Text>: 
+              <Text style={styles.output} numberOfLines={25}>
+              {promptResponse}
+              </Text> }
+
+
+              <Button
+                title="C'est bien mon objectif"
+                color='green'
+                onPress={() => handleObjectiveSend()}
+              />
+            </View>
       </View>
     );
-  }
+  };
 
 const styles = StyleSheet.create({
+  validateButoon: {
+    position: 'absolute',
+    bottom: 40
+  },
   writeTaskWrapper: {
     position: 'absolute',
     bottom: 60,
@@ -39,9 +117,14 @@ const styles = StyleSheet.create({
     borderRadius: 60,
     borderColor: '#C0C0C0',
     borderWidth: 1,
-    width: 250,
+    width: 280,
   },
-  addWrapper: {
+  output: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+  },
+  submitButton: {
     width: 60,
     height: 60,
     backgroundColor: '#FFF',
