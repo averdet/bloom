4099dee87aec8671616f2df498477682136774ca
RyanKaoukeb <90752906+RyanKaoukeb@users.noreply.github.com>
2023-02-05 06:24:19 -0500

    ProgressT

--- MyApp/components/Progress.js
+++ MyApp/components/Progress.js
@@ -8,29 +8,29 @@ function ProgressVue() {
     const now = 0.60;
     const level= 7;
     return (
-      <ImageBackground blurRadius={6}  source={require('../assets/mountain.jpg')} resizeMode={'cover'}style={homeStyle.image}>
+      <View style={{ padding : 10,flex: 1 , alignSelf : 'center'}}>
           <View style={{ padding : 10,flex: 1 , alignSelf : 'center'}}>
             <Text style={homeStyle.title}>LEVEL {level}</Text>
-            <Progress.Bar  style={{  alignSelf: 'center'}} animationType='decay' borderWidth={3} borderColor={'black'} color={'white'} progress={now} width={300} animated={true} height={40}/>
+            <Progress.Bar  style={{  alignSelf: 'center'}} animationType='decay' borderWidth={3} borderColor={'black'} color={'grey'} progress={now} width={300} animated={true} height={40}/>
             <Text style={homeStyle.subtitle}>{now*100}%</Text>
             
           </View>
           <View style={{ padding : 30,flex: 2 , alignSelf : 'center', marginLeft: 10, marginRight: 10}}>
             <View style={{ padding : 10,flex: 1 , alignSelf : 'center', flexDirection:'row'}}>
-              <Progress.Circle  thickness={8} borderWidth={0.2} borderColor={'grey'} color={'white'} size={80} progress={0.27} showsText={true}/>
+              <Progress.Circle  thickness={8} borderWidth={0.2} borderColor={'grey'} color={('blue')} size={80} progress={0.27} showsText={true}/>
               <Text style={homeStyle.subtitlepadw}>Complétion des tâches quotidiennes</Text>
             </View>
             <View style={{ padding : 10,flex: 1 , alignSelf : 'center', flexDirection:'row'}}>
-              <Progress.Circle thickness={8} borderWidth={0.2} borderColor={'grey'} color={'white'} size={70} progress={0.62} showsText={true}/>
+              <Progress.Circle thickness={8} borderWidth={0.2} borderColor={'grey'} color={'green'} size={70} progress={0.62} showsText={true}/>
               <Text style={homeStyle.subtitlepadw}>Productivité moyenne sur la journée</Text>
             </View>
             <View style={{ padding : 10,flex: 1, alignSelf : 'center', flexDirection:'row'}}>
-              <Progress.Circle thickness={8} borderWidth={0.2} borderColor={'grey'} color={'white'} size={70} progress={0.85} showsText={true}/>
+              <Progress.Circle thickness={8} borderWidth={0.2} borderColor={'grey'} color={'red'} size={70} progress={0.85} showsText={true}/>
               <Text style={homeStyle.subtitlepadw}>Productivité </Text>
             </View>
           </View>
-        
-      </ImageBackground>
+          </View>
+      
     );
   }
 
@@ -63,11 +63,11 @@ const homeStyle = StyleSheet.create({
   },
   subtitlepadw:{
     padding: 30,
-    color:'#fffffe',
+    color:'#282828',
     fontWeight:'600',
     textAlign: 'center',
     textAlignVertical: 'top',
-    fontSize: 15,
+    fontSize: 18,
   },
   image: {
     flex: 1,
