95bb1910749923e06bda1e6267c8bf0d251feec7
Swendart <sophie.curinier@etu.emse.fr>
2023-02-04 19:51:49 -0500

    Add styleSheet

---
+++ MyApp/style.js
@@ -0,0 +1,24 @@
+'use strict';
+import { StyleSheet } from 'react-native';
+
+const page = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 24,
+      backgroundColor: '#fff',
+    },
+    title: {
+      marginTop: 16,
+      paddingVertical: 8,
+      borderWidth: 4,
+      borderColor: '#20232a',
+      borderRadius: 6,
+      backgroundColor: '#61dafb',
+      color: '#20232a',
+      textAlign: 'center',
+      fontSize: 30,
+      fontWeight: 'bold'
+    },
+  });
+
+export{page}
