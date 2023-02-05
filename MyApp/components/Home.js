310f48fde9168e5bb9415d8356ebcf04a5252ee9
Swendart <sophie.curinier@etu.emse.fr>
2023-02-05 01:26:32 -0500

    Relooking home page

--- MyApp/components/Home.js
+++ MyApp/components/Home.js
@@ -1,12 +1,14 @@
 import * as React from 'react';
-import { ProgressViewIOSComponent, Text, View } from 'react-native';
+import { ProgressViewIOSComponent, Text, View, StyleSheet, ImageBackground } from 'react-native';
 import { FloatingAction } from "react-native-floating-action";
 import ProjectVue from './NewProject';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {COLORS} from '../style.js';
+
 
 
 function DefaultHomeVue({ navigation }) {
-  const name = "Bob";
+  const name = "Jeanne";
   const actions = [
     {
       text: "Nouveau projet",
@@ -22,16 +24,19 @@ function DefaultHomeVue({ navigation }) {
     }
   ];
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Bonjour {name} !</Text>
+    <ImageBackground source={require('../assets/mountain.jpg')} resizeMode={'cover'}style={homeStyle.image}>
+    <View style={{ flex: 1, alignItems: 'center'}}>
+      <Text style={homeStyle.title}>Bonjour {name} !</Text>
+      <Text style={homeStyle.subtitle}>Prête à atteindre tes objectifs ?</Text>
       <FloatingAction
         actions={actions}
+        style={{backgoundColor:'red'}}
         onPressItem={name => {
           navigation.navigate("Nouveau Projet");
-          console.log(`selected button: ${name}`);
         }}
       />
     </View>
+    </ImageBackground>
   );
 }
 
@@ -40,10 +45,33 @@ const HomeStack = createNativeStackNavigator();
 function HomeStackVue() {
   return (
     <HomeStack.Navigator>
-      <HomeStack.Screen name="Accueil" component={DefaultHomeVue} />
+      <HomeStack.Screen name="Accueil" component={DefaultHomeVue}/>
       <HomeStack.Screen name="Nouveau Projet" component={ProjectVue} />
     </HomeStack.Navigator>
   );
 }
 
 export default HomeStackVue;
+
+const homeStyle = StyleSheet.create({
+  title: {
+    color:'#2e294e',
+    fontWeight:'900',
+    textAlign: 'left',
+    border: '1px solid red',
+    fontSize: 25,
+    textAlignVertical:'top',
+    verticalAlign:'top',
+    marginTop: 60
+  },
+  subtitle:{
+    color:'#2e294e',
+    fontWeight:'600',
+    textAlign: 'left',
+    fontSize: 20,
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+});
